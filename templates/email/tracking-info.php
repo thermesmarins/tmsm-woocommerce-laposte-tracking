f86f063c5ca52fcc16cf61a127e16191bd8aa4c5
Nico Mollet <nico.mollet@gmail.com>
2019-06-28 10:21:08 +0200

    Tracking information only displays one item

--- templates/email/tracking-info.php
+++ templates/email/tracking-info.php
@@ -14,15 +14,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( $tracking_items ) : ?>
 	<h2><?php echo apply_filters( 'woocommerce_la_poste_tracking_my_orders_title',
-			__( 'Tracking Information', 'tracking-la-poste-for-woocommerce' ) ); ?></h2>
+			__( 'Tracking Information', 'tmsm-woocommerce-laposte-tracking' ) ); ?></h2>
 	<?php
+
+	$counter = 0;
 	foreach ( $tracking_items as $tracking_item ) {
+		$counter ++;
+		if ( $counter === 1 ) {
+			?>
+			<p><?php printf( __( 'Your product has been shipped. The tracking number is: %s', 'tmsm-woocommerce-laposte-tracking' ),
+					'<a href="' . esc_url( $tracking_item['formatted_tracking_link'] ) . '">' . esc_html( $tracking_item['tracking_number'] )
+					. '</a>' ); ?>
+			</p>
 
-		?>
-		<p><?php printf(__( 'Your product has been shipped. The tracking number is: %s', 'tmsm-woocommerce-laposte-tracking' ), '<a href="'.esc_url( $tracking_item[ 'formatted_tracking_link' ] ).'">'.esc_html( $tracking_item['tracking_number'] ).'</a>'); ?>
-		</p>
-
-		<?php
+			<?php
+		}
 	}
 	?>
 
